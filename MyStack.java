8d4c2cafed49c8ed5f41e6f0e2018aae53208653
unknown <nituldeori63@gmail.com>
2020-12-06 18:51:25 +0530

    Added: Ability to create a simple stack using linked list

---
+++ MyStack.java
@@ -0,0 +1,34 @@
+package com.linkedlist;
+
+public class MyStack {
+	private final MyLinkedList myLinkedList;
+	public MyStack() {
+		this.myLinkedList=new MyLinkedList();
+	}
+	
+	public MyLinkedList getMyLinkedList() {
+		return myLinkedList;
+	}
+	public void push(MyNode myNode) {
+		myLinkedList.add(myNode);
+	}
+	
+	public MyNode peak() {
+		return myLinkedList.head;
+	}
+	
+	public void pop() {
+		myLinkedList.pop();
+	}
+	public static void main(String[] args) {
+		MyNode Node1=new MyNode(70);
+		MyNode Node2=new MyNode(30);
+		MyNode Node3=new MyNode(56);
+		MyStack myStack=new MyStack();
+		myStack.push(Node1);
+		myStack.push(Node2);
+		myStack.push(Node3);
+		myStack.getMyLinkedList().printLinkedList();
+	}
+
+}
