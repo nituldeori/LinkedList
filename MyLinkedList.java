5d5734227c211e7382a1dd51ff330781650d4d12
unknown <nituldeori63@gmail.com>
2020-12-06 18:06:49 +0530

    Added: Ability to delete a node with given value from the linkedlist

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -105,6 +105,41 @@ public class MyLinkedList {
     	}
     	System.out.println("No node with given value found");
     }
+    
+    public void deleteGivenNodeWithValue(int value) {
+    	if(head==null) {
+    		System.out.println("Linked List is empty");
+    	}
+    	else if(head.getKey()==value) {
+    		head=head.getNext();
+    	}
+    	else {
+    		MyNode temp=head;
+    		MyNode prev=null;
+    		while(temp!=null && temp.getKey()!=value) {
+    			prev=temp;
+    			temp=temp.getNext();
+    			
+    		}
+    		if(temp==null) {
+    			System.out.println("No node with given key is found");
+    		}
+    		else {
+    			prev.setNext(temp.getNext());
+    		}
+    	}
+
+    }
+    
+    public int findSizeOfLinkedList() {
+    	MyNode temp=head;
+    	int size=0;
+    	while(temp!=null) {
+    		size++;
+    		temp=temp.getNext();
+    	}
+    	return size;
+    }
     public static void main(String[] args) {
 		MyLinkedList ll=new MyLinkedList();
 		MyNode node1=new MyNode(56);
@@ -116,8 +151,9 @@ public class MyLinkedList {
 		node1.setNext(node2);
 		node2.setNext(node3);
 		ll.printLinkedList();
-		ll.insertAfterGivenNodeValue(30,40);
+		ll.deleteGivenNodeWithValue(30);
 		ll.printLinkedList();
+		System.out.println(ll.findSizeOfLinkedList());
 	}
 
 }
--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -37,5 +37,20 @@ public class MyLinkedListTest {
 		int pos=ll.searchNodeWithValue(40);
 		assertEquals(3,pos);
 	}
+	
+	@Test
+	public void testdeleteGivenNodeWithValue() {
+		MyLinkedList ll=new MyLinkedList();
+		MyNode node1=new MyNode(56);
+		MyNode node2=new MyNode(30);
+		MyNode node3=new MyNode(70);
+		ll.append(node1);
+		ll.append(node2);
+		ll.append(node3);
+		ll.printLinkedList();
+		ll.deleteGivenNodeWithValue(30);
+		ll.printLinkedList();
+		assertEquals(2,ll.findSizeOfLinkedList());
+	}
 
 }
